ae0e0146367657b897ef6e15c929cd6283ffef31
perkinson1251 <tttrashutin@gmail.com>
2025-04-14 15:48:03 +0300

    feat: restructure app layout and add initial site configuration

---
+++ layers/site/nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  dir: {
+    public: 'public',
+  },
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  extends: ['./core', './layers/site'],
+  imports: {
+    dirs: ['./core/types', './layers/**/types'],
+  },
+  future: {
+    compatibilityVersion: 4,
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
