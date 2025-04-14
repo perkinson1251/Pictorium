e0017bea564cee1b252c9592eb40afc6d7957203
perkinson1251 <tttrashutin@gmail.com>
2025-04-14 16:30:34 +0300

    feat: update eslint and prettier configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,20 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 export default withNuxt(eslintPluginPrettierRecommended, {
   rules: {
     "vue/multi-word-component-names": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        plugins: [
+          "prettier-plugin-classnames",
+          "prettier-plugin-tailwindcss",
+          "prettier-plugin-merge",
+        ],
+        tailwindFunctions: ["clsx", "cn"],
+        printWidth: 100,
+        useTabs: false,
+        semi: true,
+        singleQuote: false,
+      },
+    ],
   },
 });
