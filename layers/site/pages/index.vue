253acfa2ab8ee158c2d519006fba3c11eb69afaf
perkinson1251 <tttrashutin@gmail.com>
2025-04-15 11:17:29 +0300

    feat: add all basic modules

--- layers/site/pages/index.vue
+++ layers/site/pages/index.vue
@@ -1,12 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div
-    class="font-regular active:shadow-fiord-950/15 ue-200 disabled:text-royal-blue-600
-      dark:disabled:bg-royal-blue-950 dark:disabled:text-royal-blue-300 box-border inline-flex w-auto
-      min-w-32 select-none items-center justify-center gap-2 rounded-lg px-4 text-sm outline-none
-      transition-all active:shadow-inner disabled:cursor-not-allowed disabled:shadow-none [&_svg]:size-5"
-  >
+  <div>
     <h1>Hello world!</h1>
+    <Button>Test button</Button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
@@ -9,5 +10,30 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
-  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@vueuse/nuxt",
+    // "@nuxtjs/supabase",
+    "@nuxtjs/color-mode",
+    "shadcn-nuxt",
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ['~/core/assets/css/main.css'],
+  shadcn: {
+    prefix: '',
+    componentDir: './core/components/ui'
+  },
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode',
+  },
 });
